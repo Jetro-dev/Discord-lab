249ae6fcccde6ef1077206b9aaba52f8c0cb16ec
jetro <jetrosebaxcode@gmail.com>
2026-05-06 19:22:06 -0600

    Modulo de sensores

---
+++ practica01/modulo_sensores.py
@@ -0,0 +1,84 @@
+def leer_sensores():
+    sensores = []
+
+    for i in range(5):
+        distancia = float(input(f"Ingrese distancia sensor {i+1}: "))
+        sensores.append(distancia)
+
+    return sensores
+
+
+def calcular_promedio(sensores):
+    promedio = sum(sensores) / len(sensores)
+    return promedio
+
+
+def leer_matriz():
+    camara_ia = []
+
+    print("\nLlenando matriz de cámara 3x3:\n")
+
+    for fila in range(3):
+        fila_datos = []
+
+        for col in range(3):
+            valor = int(input(f"Fila {fila}, Col {col} (Brillo 0-255): "))
+
+            # Saturación
+            if valor > 255:
+                valor = 255
+
+            fila_datos.append(valor)
+
+        camara_ia.append(fila_datos)
+
+    return camara_ia
+
+
+def imprimir_matriz(camara_ia):
+    print("\nVisualización de la imagen capturada:\n")
+
+    for fila in camara_ia:
+        print("[ ", end="")
+
+        for valor in fila:
+            print(f"{valor:3}", end=" ")
+
+        print("]")
+
+
+def contar_brillantes(camara_ia):
+    contador = 0
+
+    for fila in camara_ia:
+        for valor in fila:
+            if valor > 200:
+                contador += 1
+
+    return contador
+
+
+def main():
+    print("--- MÓDULO DE SENSORES (VECTORES) ---\n")
+
+    sensores = leer_sensores()
+    promedio = calcular_promedio(sensores)
+
+    if promedio < 2.0:
+        print(f"\nPromedio de proximidad: {promedio:.1f}m. Aviso: Reduciendo velocidad global.")
+    else:
+        print(f"\nPromedio de proximidad: {promedio:.1f}m. Estado: Seguro.")
+
+    print("\n--- MÓDULO DE VISIÓN (MATRICES) ---")
+
+    camara_ia = leer_matriz()
+
+    imprimir_matriz(camara_ia)
+
+    brillantes = contar_brillantes(camara_ia)
+
+    print("\nResultado de Análisis IA:")
+    print(f"Se detectaron {brillantes} píxeles de alta intensidad.")
+
+if __name__ == "__main__":
+    main()
